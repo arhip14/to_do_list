645e24cff9daffd2bfdcaeead92f2d4f557e542e
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2026-01-06 03:01:13 +0200

    add service and controller

---
+++ src/main/java/com/app/demo/services/TaskService.java
@@ -0,0 +1,21 @@
+package com.app.demo.services;
+
+import com.app.demo.models.Task;
+import com.app.demo.repository.TaskRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+}
