24bd0f8c8eff024965d62c9d00789c61d1e4c6fd
arkhypkryvdiuk <kruvdyk1503@gmail.com>
2026-01-16 17:12:13 +0200

    add task func

--- src/main/java/com/app/demo/controller/TaskController.java
+++ src/main/java/com/app/demo/controller/TaskController.java
@@ -5,7 +5,9 @@ import com.app.demo.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -22,6 +24,12 @@ public class TaskController  {
         List<Task> tasks = taskService.getAllTasks();
         model.addAttribute("tasks", tasks);
         return "tasks";
+    }
+
+    @PostMapping
+    public String createTasks(@RequestParam String title) {
+      taskService.createTask(title);
+        return "redirect:/";
     }
 }
 
